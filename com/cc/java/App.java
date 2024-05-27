29e771d09d579e5922f9efc52b05770e48b49a77
Phl0h_CC <phloh@gmx.de>
2024-05-27 11:56:27 +0200

    Overriding comments

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,9 +4,10 @@ public class App {
     
     public static void main(String[] args) {
      
+       Child child = new Child();
+       output(child.sayHello()); // sucht zuerst in Child class und wenn die Methode existiert wird sie ausgeführt
+       output(child.sayHello("Holler!")); // wenn keine passende Methode gefunden wird, geht es in der Hirarchie weiter (zur nächsten superClass)
        
-       
-
     }
 
     private static void output(String outputStr) {
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Parent {
+    
+    public String sayHello(){
+        return "Hello from parent!";
+    }
+
+    public String sayHello(String string){
+        return string;
+    }   
+
+}
